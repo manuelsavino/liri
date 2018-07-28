293349e68da94c128fe4ed67abb564a7f601dfb7
Manuel Savino <manuelsavino@gmail.com>
2018-07-27 20:45:32 -0400

    all functions working

--- liri.js
+++ liri.js
@@ -1,33 +1,39 @@
 var keys = require("./keys")
 var Twitter = require('twitter')
 var Spotify = require('node-spotify-api')
-require('requester')
-
-
-
-// console.log(keys.spotify.id)
+var request = require('request')
+var fs = require("fs")
 
 var query = process.argv
 query = query.slice(2)
 
-switch(query[0])
-{
+switch (query[0]) {
   case 'my-tweets':
-  callTwitter()
-  break;
+    callTwitter()
+    break;
 
   case 'spotify-this-song':
-  var song = query.slice(1).join(" ")
-  callSpotify(song)
-  break;
+    var song = query.slice(1).join(" ")
+    callSpotify(song)
+    break;
+
+  case 'movie-this':
+    var movie = query.slice(1).join("+")
+    if (!movie)
+      callOmdb("Mr Nobody")
+    else
+      callOmdb(movie)
+    break;
+
+  case "do-what-it-says":
+    doWhatIsSays()
+    break;
 
 }
-
-function callSpotify(song)
-{
-  var spotify = new Spotify({id: keys.spotify.id, secret: keys.spotify.secret})
-  spotify.search({type: 'track', query: song,}, function(err, data){
-    if(err){
+function callSpotify(song) {
+  var spotify = new Spotify({ id: keys.spotify.id, secret: keys.spotify.secret })
+  spotify.search({ type: 'track', query: song, }, function (err, data) {
+    if (err) {
       console.log(err)
     }
     var data = data.tracks.items[0]
@@ -37,18 +43,18 @@ function callSpotify(song)
     var url = data.external_urls.spotify
     var artistNames = []
     artist.forEach(element => {
-    artistNames.push(element.name)
-  });
-      
-    console.log("Artist(s): " + artistNames.join(", "))
-    console.log(`Song Name: ${song}`)
-    console.log(`Album: ${album}`)
-    console.log(`URL: ${url}`)
+      artistNames.push(element.name)
+    });
+    console.log(" ----------------------------")
+    console.log("| Artist(s): " + artistNames.join(", "))
+    console.log(`| Song Name: ${song}`)
+    console.log(`| Album: ${album}`)
+    console.log(`| URL: ${url}`)
+    console.log(" ----------------------------")
   })
 }
 
-function callTwitter()
-{
+function callTwitter() {
   var client = new Twitter({
     consumer_key: keys.twitter.consumer_key,
     consumer_secret: keys.twitter.consumer_secret,
@@ -56,14 +62,58 @@ function callTwitter()
     access_token_secret: keys.twitter.access_token_secret
   });
 
-  client.get('statuses/user_timeline', { screen_name: 'manuel17', count: 20 },function(error, tweets) {
-    if(error) throw error;
+  client.get('statuses/user_timeline', { screen_name: 'manuel17', count: 20 }, function (error, tweets) {
+    if (error) throw error;
     // console.log(tweets[0].text);  // The favorites.
     // console.log(response);  // Raw response object.
-    tweets.forEach(function(element){
+    tweets.forEach(function (element) {
       console.log("------------------------------")
-      console.log(element.text)
-      console.log(element.created_at)
+      console.log(`${element.text}`)
+      console.log(`${element.created_at}`)
     })
   });
+}
+
+function callOmdb(movieName) {
+  var client = keys.omdb.key
+  request(`http://www.omdbapi.com/?i=tt3896198&apikey=${client}&type=movie&t=${movieName}`, (error, response, body) => {
+    // console.log(JSON.stringify(body))
+    data = JSON.parse(body)
+    console.log(" -----------------------------")
+    console.log(`| Title: ${data.Title}`)
+    console.log(`| Year: ${data.Year}`)
+    console.log(`| IMDB Rating: ${data.imdbRating}`)
+    console.log(`| Rotten Tomatoes Rating: ${data.Ratings[1].Value}`)
+    console.log(`| Country of Production: ${data.Country}`)
+    console.log(`| Language: ${data.Language}`)
+    console.log(`| Plot: ${data.Plot}`)
+    console.log(`| Actors: ${data.Actors}`)
+    console.log(" -----------------------------")
+
+  })
+}
+
+function doWhatIsSays() {
+  fs.readFile("random.txt", "utf8", (err, data) => {
+    var command = data.split(" ")
+    switch (command[0]) {
+      case 'my-tweets':
+        callTwitter()
+        break;
+
+      case 'spotify-this-song':
+        var song = command.slice(1).join(" ")
+        callSpotify(song)
+        break;
+
+      case 'movie-this':
+        var movie = command.slice(1).join("+")
+        if (!movie)
+          callOmdb("Mr Nobody")
+        else
+          callOmdb(movie)
+        break;
+    }
+  })
+
 }
